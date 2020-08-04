0118142cc1fe3f627d891984a1909702e3c9e372
Aman Khandelwal <amank@oakstree.com>
2020-08-04 15:06:46 +0530

    file open option added

--- m.py
+++ m.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter.filedialog import asksaveasfile
+from tkinter.filedialog import asksaveasfile,askopenfilename
 
 
 class DEditor:
@@ -17,11 +17,22 @@ class DEditor:
         file.write(inputValue)
 
 
+    def openFile(self):
+        files = [('All Files', '*.*'), ('Python Files', '*.py'), ('Text Document', '*.txt')]
+        file =  askopenfilename(initialdir = "/",title = "Select file",filetypes = files)
+        filepath = file
+        with open(filepath,'r') as d:
+            data = d.read()
+        self.text.insert("end-1c",data)
+
+
     def execute(self):
         self.text = Text(self.root)
         self.text.pack()
         buttonCommit=Button(self.root, height=1, width=10, text="Save File", command=lambda: self.retrieve_input())
         buttonCommit.pack()
+        buttonOpen=Button(self.root, height=1, width=10, text="Open File", command=lambda: self.openFile())
+        buttonOpen.pack()
         self.root.title('DEditor')
         self.root.mainloop()
 
