f27d7974c9f35ab73e23f5f9fda7b1cb60f43103
Aman Khandelwal <amank@oakstree.com>
2020-08-05 09:02:43 +0530

    Menubar added

--- m.py
+++ m.py
@@ -14,25 +14,44 @@ class DEditor:
         print(inputValue)
         files = [('All Files', '*.*'), ('Python Files', '*.py'), ('Text Document', '*.txt')] 
         file = asksaveasfile(filetypes = files, defaultextension = files)
-        file.write(inputValue)
+        if file!= None:
+            file.write(inputValue)
 
 
     def openFile(self):
         files = [('All Files', '*.*'), ('Python Files', '*.py'), ('Text Document', '*.txt')]
         file =  askopenfilename(initialdir = "/",title = "Select file",filetypes = files)
         filepath = file
-        with open(filepath,'r') as d:
-            data = d.read()
-        self.text.insert("end-1c",data)
+        print(filepath)
+        if filepath != '':
+            with open(filepath,'r') as d:
+                data = d.read()
+            self.text.insert("end-1c",data)
 
 
     def execute(self):
         self.text = Text(self.root)
         self.text.pack()
-        buttonCommit=Button(self.root, height=1, width=10, text="Save File", command=lambda: self.retrieve_input())
-        buttonCommit.pack()
-        buttonOpen=Button(self.root, height=1, width=10, text="Open File", command=lambda: self.openFile())
-        buttonOpen.pack()
+        #buttonCommit=Button(self.root, height=1, width=10, text="Save File", command=lambda: self.retrieve_input())
+        #buttonCommit.pack()
+        #buttonOpen=Button(self.root, height=1, width=10, text="Open File", command=lambda: self.openFile())
+        #buttonOpen.pack()
+        menubar = Menu(self.root)
+        filemenu = Menu(menubar, tearoff = 0)
+        filemenu.add_command(label="Save", command = self.retrieve_input)
+        filemenu.add_separator()
+        filemenu.add_command(label = "Exit", command = self.root.quit)
+        
+        menubar.add_cascade(label = "File", menu = filemenu)
+        
+        helpmenu = Menu(menubar, tearoff=0)
+        helpmenu.add_command(label = "Open File", command = self.openFile)
+        helpmenu.add_command(label = "About", command = self.openFile)
+        
+        menubar.add_cascade(label = "Help", menu = helpmenu)
+        
+        
+        self.root.config(menu=menubar)
         self.root.title('DEditor')
         self.root.mainloop()
 
